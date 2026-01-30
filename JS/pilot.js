7452d7f244fb5322fa78c694ec50586e70d01abc
aaryandidwania <aaryand2220@gmail.com>
2026-01-30 22:18:30 +0530

    modified:   JS/pilot.js

--- JS/pilot.js
+++ JS/pilot.js
@@ -1,7 +1,21 @@
 const myObj = {
     'name' : "Aaryan",
-    'age' : 18
+    'age' : 19
     
 }
 
 console.log(myObj.age);
+
+const myObj2 = {
+    info: {
+        'names' : ['Aaryan' , 'Arnav' , 'Shourya'],
+        'ages' : [19 , 20 , 21] 
+    },
+
+    addresses : ['Noida sector-45' , 'Rohini sector-21' , 'Noida sector-62']
+
+}
+
+console.log(myObj2.info.names[0]); //gives us 'Aaryan'...
+
+
