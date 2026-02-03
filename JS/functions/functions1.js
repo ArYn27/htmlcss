c4e50cf7b5371c81855cc55f63f85e3ba62f48d1
ArYn27 <aaryand2220@gmail.com>
2026-02-04 01:23:09 +0530

    new file:   JS/12-js-oops/12-js-oops/part6/contructorFunction.js 	new file:   JS/12-js-oops/12-js-oops/part6/oop-master.js 	new file:   JS/12-js-oops/12-js-oops/part6/prototypes.js 	new file:   JS/12-js-oops/__MACOSX/._12-js-oops 	new file:   JS/12-js-oops/__MACOSX/12-js-oops/._part6 	new file:   JS/12-js-oops/__MACOSX/12-js-oops/part6/._contructorFunction.js 	new file:   JS/12-js-oops/__MACOSX/12-js-oops/part6/._oop-master.js 	new file:   JS/12-js-oops/__MACOSX/12-js-oops/part6/._prototypes.js 	modified:   JS/functions/functions1.js

--- JS/functions/functions1.js
+++ JS/functions/functions1.js
@@ -195,4 +195,25 @@ console.log(k);
   
  let g = countVowels("Aaryan")
  console.log(g);
- 
+
+
+const person = {
+    name : "Hitesh",
+    age : 19.5,
+    introduce(){
+        return `Hi my name is ${this.name} and I am ${this.age} years old`
+    }
+};
+
+
+
+
+function outer() {
+
+    function inner() {
+        return "Inner function called"
+    }
+    return inner();
+}
+
+console.log(outer());
