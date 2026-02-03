93135c3e8b7bc8bd9ba5fc233c48548d5d47f976
ArYn27 <aaryand2220@gmail.com>
2026-02-04 01:47:22 +0530

    modified:   JS/12-js-oops/12-js-oops/part6/prototypes.js

--- JS/12-js-oops/12-js-oops/part6/prototypes.js
+++ JS/12-js-oops/12-js-oops/part6/prototypes.js
@@ -7,12 +7,15 @@ let tomHardware = {};
 
 // console.log(`lenovo `, lenovo.__proto__);
 
-let genericCar = { tyres: 4 };
+let genericCar = { tyres: 4 , batteryPack : 650 };
 
 let tesla = {
+
   driver: "AI",
 };
 
 Object.setPrototypeOf(tesla, genericCar);
 
 console.log(`tesla `, Object.getPrototypeOf(tesla));
+
+
