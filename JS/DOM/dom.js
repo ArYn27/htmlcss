30d36aaf745d75883a368b216cd50b4ad21cf026
ArYn27 <aaryand2220@gmail.com>
2026-02-04 13:46:00 +0530

    new file:   JS/DOM/dom.js 	new file:   JS/DOM/index.html

---
+++ JS/DOM/dom.js
@@ -0,0 +1,8 @@
+console.log("hello from script");
+console.log(window);
+console.log(document.firstChild);
+console.log(document.firstChild.firstChild);
+console.log(document.getElementsByTagName("h1"));
+
+//DOM = DOCUMENT OBJECT MODEL
+//BOM = BROWSER OBJECT MODEL
