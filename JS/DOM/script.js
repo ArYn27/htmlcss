f056b3f83017b0e40a6543d96f962a05478fc3c3
aaryandidwania <aaryand2220@gmail.com>
2026-02-12 00:20:57 +0530

    modified:   JS/DOM/dom.html 	modified:   JS/DOM/script.js

--- JS/DOM/script.js
+++ JS/DOM/script.js
@@ -31,8 +31,26 @@ document
     //example 2
 
     document.getElementById('highlightFirstCity')
-    .addEventListener('cllick' , function(){
+    .addEventListener('click' , function(){
         let citiesList = document.getElementById("citiesList")
-        console.log(citiesList.firstElementChild);
+        citiesList.firstElementChild.classList.add('highlight')
          
+    })
+
+    //example 3
+
+    document.getElementById("changeOrder").addEventListener('click' , function () {
+        let coffeeType = document.getElementById("coffeeType")
+        coffeeType.textContent = "Espresso"
+        coffeeType.style.backgroundColor = "brown"
+        coffeeType.style.padding = "5px"
+    })
+
+    //example 4
+
+    document.getElementById("addNewItem").addEventListener('click' , function(){
+        let newItem = document.createElement('li')
+        newItem.textContent = "Eggs"
+
+        document.getElementById("shoppingList").appendChild(newItem)
     })
