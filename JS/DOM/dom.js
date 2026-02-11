37c2564eb9c58789a0f276a7395b43d7dce6e3b5
ArYn27 <aaryand2220@gmail.com>
2026-02-11 17:25:51 +0530

    modified:   JS/DOM/dom.js

--- JS/DOM/dom.js
+++ JS/DOM/dom.js
@@ -1,8 +1,6 @@
 console.log("hello from script");
 console.log(window);
-console.log(document.firstChild);
-console.log(document.firstChild.firstChild);
-console.log(document.getElementsByTagName("h1"));
+
 
 //DOM = DOCUMENT OBJECT MODEL
-//BOM = BROWSER OBJECT MODEL
+//BOM = BROWSER OBJECT MODEL 
