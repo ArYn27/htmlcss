45f8e2de65ef924df74ae18d3e4db4342b66f7da
ArYn27 <aaryand2220@gmail.com>
2026-02-11 21:56:29 +0530

    new file:   JS/DOM/dom.html 	modified:   JS/DOM/dom.js 	modified:   JS/DOM/index.html 	new file:   JS/DOM/script.js

--- JS/DOM/dom.js
+++ JS/DOM/dom.js
@@ -1,5 +1,5 @@
 console.log("hello from script");
-console.log(window);
+console.log(document.getElementsByTagName("h1"));
 
 
 //DOM = DOCUMENT OBJECT MODEL
---
+++ JS/DOM/script.js
@@ -0,0 +1,28 @@
+console.log("hello from script");
+
+//example 1
+//grab the event first
+//in query selector , use "#" for the id , "." for the classes
+//document.querySelector('') this is not ideal to use
+// document.querySelectorAll select all elements with the pattern
+/*document
+    .getElementById('changeTextButton')
+    .addEventListener('click' , function () {
+        let paragraph = document.getElementById('myParagraph')
+        paragraph.textContent = "the paragraph has been changed"
+        
+    })
+
+*/
+ /*   EventListener takes two parameters as input (event , function)
+    event being the behaviour performed before the function is called on
+    a certain element of the webpage . here , we use normal functions not 
+    arrow functions.
+*/
+
+document
+    .getElementById('changeTextButton')
+    .addEventListener('click' , function(){
+        let paragraph = document.getElementById('myParagraph')
+        paragraph.textContent = "this paragraph has been changed"
+    });
