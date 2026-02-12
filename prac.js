aa5c087e2678f8e1c471e599b7045e8d9b6b1f37
aaryandidwania <aaryand2220@gmail.com>
2026-02-12 12:33:43 +0530

    modified:   index.html 	modified:   prac.js

--- prac.js
+++ prac.js
@@ -14,3 +14,15 @@ document.getElementById("btn").addEventListener('click' , function(){
         error.style.fontSize = "10px"
     }
 })
+
+
+let obj = {
+    name : "aaryan",
+    place : "noida",
+    age : 19,
+    cgpa : 7.1,
+    leetcode : "550+",
+    codeforces : "2400"
+}
+
+console.log(Object.values(obj)[1]);
